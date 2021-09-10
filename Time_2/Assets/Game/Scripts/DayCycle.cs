d9ec4a7554085cc0f5ef713a0a92345adfac3d3b
vitorsalek <59521603+vitorsalek@users.noreply.github.com>
2021-09-09 23:43:27 -0300

    Day Cycle melhorado

--- Time_2/Assets/Game/Scripts/DayCycle.cs
+++ Time_2/Assets/Game/Scripts/DayCycle.cs
@@ -1,23 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DayCycle : MonoBehaviour
 {
-    public Transform sun;
-    public float speed;
+    [Range(0.0f, 1.0f)]
+    public float time;
+    public float fullDayLenght;
+    public float startTime = 0.4f;
+    private float timeRate;
+    public Vector3 noon;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Sun")]
+    public Light sun;
+    public Gradient sunColor;
+    public AnimationCurve sunIntensity;
+
+    [Header("Moon")]
+    public Light moon;
+    public Gradient moonColor;
+    public AnimationCurve moonIntensity;
+
+    [Header("Lighting effects")]
+    public AnimationCurve lightingIntensityMultiplier;
+    public AnimationCurve reflectionsIntensityMultiplier;
+
+    public TextMeshProUGUI Time_txt;
+
+    private void Start()
     {
-        RotateSun();
+        timeRate = 1.0f / fullDayLenght;
+        time = startTime;
     }
 
-
-    void RotateSun()
+    private void Update()
     {
-        float timer = Time.deltaTime;
-        sun.transform.Rotate(timer * speed, 0, 0);
-        timer = 0f;
+        setTime();
+        lightController();
     }
+
+    void setTime()
+    {
+        //incrementa tempo
+        time += timeRate * Time.deltaTime;
+        if (time >= 1.0f)
+            time = 0f;
+        int hour = Mathf.FloorToInt(time * 24);
+        int minutes = ((int)(((time * 24) % 1) * 6))*10;
+        Time_txt.text = hour.ToString("00") + ":" + minutes.ToString("00");
+    }
+
+    void lightController()
+    {
+        //rotação da luz
+        sun.transform.eulerAngles = (time - 0.3f) * noon * 4.0f;
+        moon.transform.eulerAngles = (time - 0.75f) * noon * 4.0f;
+
+        //intensidade da luz
+        sun.intensity = sunIntensity.Evaluate(time);
+        moon.intensity = moonIntensity.Evaluate(time);
+
+        //mudança de cor
+        sun.color = sunColor.Evaluate(time);
+        moon.color = moonColor.Evaluate(time);
+
+        //ativa/desativa sol
+        if (sun.intensity <= 0 && sun.gameObject.activeInHierarchy)
+            sun.gameObject.SetActive(false);
+        else if (sun.intensity > 0 && !sun.gameObject.activeInHierarchy)
+            sun.gameObject.SetActive(true);
+
+        //ativa/desativa lua
+        if (moon.intensity <= 0 && moon.gameObject.activeInHierarchy)
+            moon.gameObject.SetActive(false);
+        else if (moon.intensity > 0 && !moon.gameObject.activeInHierarchy)
+            moon.gameObject.SetActive(true);
+
+        //intensidade do reflexo da luz
+        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+        RenderSettings.reflectionIntensity = reflectionsIntensityMultiplier.Evaluate(time);
+    }
+
 }
