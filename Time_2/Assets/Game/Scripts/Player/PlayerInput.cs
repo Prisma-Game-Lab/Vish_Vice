b2b3e2a6c793aa09219942c380fda92b5e0cdce5
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2021-09-22 18:23:26 -0300

    Implementa base para animação de caminhada do player
    Player anda em 8 direções com animações placeholder.

--- Time_2/Assets/Game/Scripts/Player/PlayerInput.cs
+++ Time_2/Assets/Game/Scripts/Player/PlayerInput.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public float speed;
+    public Animator anim;
     public VariableJoystick variableJoystick;
     private CharacterController controller;
 
@@ -16,6 +17,10 @@ public class PlayerInput : MonoBehaviour
     private void Update()
     {
         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+
+        anim.SetFloat("Horizontal", variableJoystick.Horizontal);
+        anim.SetFloat("Vertical", variableJoystick.Vertical);
+        anim.SetFloat("Moving", direction.magnitude);
         controller.Move(direction * speed * Time.deltaTime);
     }
 
